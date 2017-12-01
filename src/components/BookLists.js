2a7b88c77bf36d1371b881bee40b51117562cb3e
Carla Severe <carlaflagco@gmail.com>
2017-12-01 16:27:02 -0500

    first commit

---
+++ src/components/BookLists.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class BookLists extends Component{
+    render(){
+        return(
+            <ul className = "list-group col-sm-4">
+
+            </ul> 
+        )
+    }
+}
+
+export default BookLists;  
