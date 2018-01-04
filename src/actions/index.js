72dd0a94a5807a6c894222d868741cb270ac8ddf
csevere <carla.severe@gmail.com>
2018-01-03 23:33:29 -0500

    completed mobile for first two pages

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export function selectBook(book){
+    //selectBook is an ActionCreator, it needs to return an action
+    //an object with a type property
+
+    return{
+        type: 'BOOK_SELECTED',
+        payload: book
+
+    };
+}
+
+// action always has a type, uppercaase
+//sometimes a payload 
